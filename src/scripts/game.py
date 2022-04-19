77ea5802505c07c91887f027d1e47f5dc1a06dae
Carson Fletcher <carsontf5@gmail.com>
2022-04-18 21:37:39 -0600

    game now loads images & text from assets

--- src/scripts/game.py
+++ src/scripts/game.py
@@ -8,17 +8,30 @@ class Game:
     """Main class for creating and running a game instance."""
 
     def __init__(self) -> None:
-        self.screen: pygame.Surface
+        self.game_name = "Flappy Bird"
+        self.screen: pygame.surface.Surface
+        self.sky_surface: pygame.surface.Surface
+        self.ground_surface: pygame.surface.Surface
         self.clock: pygame.time.Clock
+        self.font: pygame.font.Font
+        self.game_active: bool = False
 
     def start(self) -> None:
         """Starts the game instance and creates a new window for the game."""
-        print(pygame.__file__)
         pygame.init()
+
+        # Init screen and game sprites
         self.screen = pygame.display.set_mode((800, 400))
-        self.screen.fill((155, 0, 200))
+        pygame.display.set_caption("Flappy Bird")
+        self.sky_surface = pygame.image.load(
+            "./src/assets/images/background/sky.png"
+        ).convert()
+        self.ground_surface = pygame.image.load(
+            "./src/assets/images/background/ground.png"
+        ).convert()
+        self.font = pygame.font.Font("src/assets/images/fonts/pixel_type.ttf", 50)
+
         self.clock = pygame.time.Clock()
-        pygame.display.set_caption("Hello, World!")
         self.update()
 
     def update(self) -> None:
@@ -27,6 +40,15 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit()
+
+            if self.game_active:
+                self.screen.blit(self.sky_surface, (0, 0))
+                self.screen.blit(self.ground_surface, (0, 300))
+            else:
+                self.screen.fill((36, 97, 227))
+                score_message = self.font.render('Your score: {}',False, (255, 255, 255))
+                score_message_rect = score_message.get_rect(center=(400, 330))
+                self.screen.blit(score_message,score_message_rect)
             pygame.display.update()
             self.clock.tick(60)
 
